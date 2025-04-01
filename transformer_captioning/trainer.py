2ef4c15af5f44b935313f81e143fcf56ef593fa5
Shivi Gupta <47184100+shiviigupta@users.noreply.github.com>
2025-03-31 22:25:50 -0400

    Update trainer.py

--- transformer_captioning/trainer.py
+++ transformer_captioning/trainer.py
@@ -24,6 +24,13 @@ class Trainer(object):
         #TODO - Compute cross entropy loss between predictions and labels. 
         #Make sure to compute this loss only for indices where label is not the null token.
         #The loss should be averaged over batch and sequence dimensions. 
+        mask = (labels != self.model._null).float()
+        predictions = predictions.view(-1, predictions.size(-1))
+        labels = labels.view(-1)
+        mask = mask.view(-1)
+        loss = torch.nn.functional.cross_entropy(predictions, labels, reduction='none')
+        loss = loss * mask
+        loss = loss.sum() / mask.sum()
         return loss
     
     def val(self):
